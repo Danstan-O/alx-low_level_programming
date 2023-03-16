5d42eb9456e6b1cf2a02260e72352353d9607533
Danstan <oduordanstan@gmail.com>
2023-03-16 14:16:08 -0700

    betty

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,9 @@
 #include<stdio.h>
-/** c is not
+/**
+*c is not
 *funny
 */
 int main(void)
 {
-  puts("\"Programming is like building a multilingual puzzle");
+puts("\"Programming is like building a multilingual puzzle");
 }
