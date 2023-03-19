04e867f2c4b0d83c89eba707b2bb9079f8dd0669
danstan <oduordanstan@gmail.com>
2023-03-19 18:14:04 +0300

    change the catch phrase

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -23,7 +23,7 @@ else if (z == 0)
 {
 printf("Last digit of %d is %d and is 0\n", n, z);
 }
-else if (z < 6 && z == 0)
+else
 {
 printf("Last digit of %d is %d and is less than 6 and not 0\n", n, z);
 }
