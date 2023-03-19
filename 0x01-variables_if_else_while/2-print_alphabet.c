33865bee46f1d0309ce5d9d3795a1897fc413c87
danstan <oduordanstan@gmail.com>
2023-03-19 19:09:32 +0300

    printing an alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+/**
+ * main - major entry point
+ * Return: returns 0
+ */
+void main(void)
+{
+char alphabet;
+for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+{
+putchar(alphabet);
+}
+putchar('\n');
+}
