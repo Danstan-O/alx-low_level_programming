1dd1b9ed9dffcadd168abff006f2ef716ab70025
danstan <oduordanstan@gmail.com>
2023-03-19 21:09:24 +0300

    escaping a character

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+/**
+ * main - major entry point
+ * Return: returns 0
+ */
+int main(void)
+{
+char i;
+for (i = 'a'; i < 'z'; i++)
+{
+if (i == 'q' || i == 'e')
+{
+}
+else
+{
+putchar(i);
+}
+}
+putchar('\n');
+return (0);
+}
