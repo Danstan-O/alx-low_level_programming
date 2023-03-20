f69ef8d3a56827573716f350da4723b96a759744
danstan <oduordanstan@gmail.com>
2023-03-20 23:15:08 +0300

    printing combos

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+/**
+ * main - major entry point
+ * Return: returns 0
+ */
+int main(void)
+{
+int i;
+int j;
+int k;
+int l;
+for (i = 0; i < 10; i++)
+for (j = 0; j < 10; j++)
+for (k = 0; k < 10; k++)
+for (l = 0; l < 10; l++)
+{
+if (j != l || k != l)
+{
+putchar((i) + '0');
+putchar((j) + '0');
+putchar(' ');
+putchar((k) + '0');
+putchar((l) + '0');
+if (i != 9 || j != 8 || k != 9 || l != 9)
+{
+putchar(',');
+putchar(' ');
+}
+}
+}
+putchar('\n');
+return (0);
+}
