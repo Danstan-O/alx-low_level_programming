98bfae9f60f6aee9c19ff5d6e7b90c06e5797d05
danstan <oduordanstan@gmail.com>
2023-03-20 21:18:09 +0300

    printing double numbers

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -10,15 +10,15 @@ int j;
 for (i = 0; i < 9; i++)
 for (j = 1; j < 10; j++)
 {
-if (i == j)
+if (i != j)
 {
-}
 putchar((i) + '0');
 putchar((j) + '0');
 if (i != 8 || j != 9)
 {
 putchar(',');
 putchar(' ');
+}
 }
 }
 putchar('\n');
