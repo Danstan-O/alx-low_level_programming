dfbb090725afc3132ba66d206fd56979f6c2b49f
danstan <oduordanstan@gmail.com>
2023-03-22 23:10:31 +0300

    printing 10 times

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,14 +1,18 @@
 #include "main.h"
 /**
- * print_alphabet - the major entry point
+ * print_alphabet_x10 - the major entry point
  * Return: returns 0
  */
 void print_alphabet_x10(void)
 {
+int times;
 char i;
-for (i = 'a'; i <= 'z'; i = i + 10)
+for (times = 0; times <= 9; times++)
+{
+for (i = 'a'; i <= 'z'; i++)
 {
 _putchar(i);
 }
 _putchar('\n');
+}
 }
