20492d902119a606b7036dfe68849bde4b3e0cf3
danstan <oduordanstan@gmail.com>
2023-03-23 15:34:06 +0300

    printing last digits

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -7,8 +7,20 @@
 int print_last_digit(int n)
 {
 int last_digit;
+int abs_num;
+if (n > 0)
+{
 last_digit = n % 10;
-_putchar(last_digit);
-_putchar('\n'); 
+return (last_digit);
+}
+else if (n < 0)
+{
+abs_num = abs(n);
+abs_num = n % 10;
+return (abs_num);
+}
+else
+{
 return (0);
+}
 }
