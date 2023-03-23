308c8ee2d69a8c33407903c57202247c9177b255
danstan <oduordanstan@gmail.com>
2023-03-23 15:50:09 +0300

    factor code

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -7,20 +7,24 @@
 int print_last_digit(int n)
 {
 int last_digit;
+last_digit = n % 10;
 int abs_num;
+int abs_number;
 if (n > 0)
 {
-last_digit = n % 10;
-return (last_digit);
+_putchar(last_digit + '0');
+return (0);
 }
 else if (n < 0)
 {
-abs_num = abs(n);
-abs_num = n % 10;
-return (abs_num);
+abs_num *= -1;
+last_digit = abs_num % 10;
+_putchar(last_digit + '0');
+return (0);
 }
 else
 {
+_putchar('0');
 return (0);
 }
 }
