3d43e8e3d09c99d081e64f13e5ba2640cb903f5e
danstan <oduordanstan@gmail.com>
2023-04-04 19:04:28 +0300

    zero

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,28 +1,12 @@
 #include "main.h"
-#include<stdio.h>
-#include<stdlib.h>
-#include<time.h>
-
 /**
- *positive_or_negative - prints if the number is negative, positive or 0
- *@i: the number generated
+ * main - our major entry point
+ * Return: returns 0
  */
-void positive_or_negative(int i)
+int main(void)
 {
-int n;
-
-srand(time(0));
-n = rand() - RAND_MAX / 2;
-if (n > 0)
-{
-printf("%d is positive\n", n);
-}
-else if (n == 0)
-{
-printf("%d is zero\n", n);
-}
-else
-{
-printf("%d is negative\n", n);
-}
+int i;
+i = 0;
+positive_or_negative(i);
+return (0);
 }
