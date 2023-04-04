669e220cf4730c09d796b42d5b632a2bf3d4db89
danstan <oduordanstan@gmail.com>
2023-04-04 15:11:57 +0300

    declared prod globally

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -7,6 +7,7 @@ void times_table(void)
 {
 int i;
 int times;
+int prod;
 for (times = 0; times < 10; times++)
 {
 _putchar('0');
@@ -16,7 +17,6 @@ for (i = 1; i < 10; i++)
 _putchar(',');
 _putchar(' ');
 
-int prod;
 prod = i * times;
 if (prod < 10)
 {
