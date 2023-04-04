e957c05d140c5eb2f30f6d8f4bf0a26a82994dc6
danstan <oduordanstan@gmail.com>
2023-04-04 15:10:36 +0300

    printing 9x9 maths table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,34 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * times_table - prints the multiplication table
+ */
+void times_table(void)
+{
+int i;
+int times;
+for (times = 0; times < 10; times++)
+{
+_putchar('0');
+
+for (i = 1; i < 10; i++)
+{
+_putchar(',');
+_putchar(' ');
+
+int prod;
+prod = i * times;
+if (prod < 10)
+{
+_putchar(' ');
+
+}
+else
+{
+_putchar((prod / 10) + '0');
+}
+_putchar((prod % 10) + '0');
+}
+_putchar('\n');
+}
+}
