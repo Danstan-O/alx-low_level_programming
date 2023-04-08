4e505885f0336d8ec77ee820b741939627c46e1f
danstan <oduordanstan@gmail.com>
2023-04-08 17:32:01 +0300

    puts file

--- 0x09-static_libraries/3-puts.c
+++ 0x09-static_libraries/3-puts.c
@@ -1,14 +1,22 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
- * _puts - the brk entry point
- * @s: the input parameter
- * Return: returns 0
+ * _puts - prints a string, followed by a new line.
+ * @s: input string.
+ * Return: no return.
  */
-
 void _puts(char *s)
 {
-puts("\"Programming is like building a multilingual puzzle");
+int count = 0;
 
+while (count >= 0)
+{
+if (s[count] == '\0')
+{
+_putchar('\n');
+break;
+}
+_putchar(s[count]);
+count++;
+}
 }
