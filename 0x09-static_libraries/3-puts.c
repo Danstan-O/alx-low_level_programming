d4b50e2431f7121d24c1e60e14e45099b05de189
danstan <oduordanstan@gmail.com>
2023-04-08 17:17:08 +0300

    generate an output

--- 0x09-static_libraries/3-puts.c
+++ 0x09-static_libraries/3-puts.c
@@ -1,12 +1,14 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
  * _puts - the brk entry point
- * @s: the first input parameter
+ * @s: the input parameter
+ * Return: returns 0
  */
 
 void _puts(char *s)
 {
-
+puts("\"Programming is like building a multilingual puzzle");
 
 }
