ffa286a5ec1b027bb3ea676d079d59fd602705ba
danstan <oduordanstan@gmail.com>
2023-04-08 17:46:41 +0300

    copying a character

--- 0x09-static_libraries/2-strncpy.c
+++ 0x09-static_libraries/2-strncpy.c
@@ -1,1 +1,14 @@
-void _puts(char *s);
+#include "main.h"
+
+/**
+ * _strncpy - the brk entry point
+ * @dest: the first input parameter
+ * @src: the second input param.
+ * @n: the third input param.
+ * Return: returns a character
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+
+}
