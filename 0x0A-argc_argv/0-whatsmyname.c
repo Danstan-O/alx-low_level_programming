c7651567931c6ac556304803c5344b18cbd90e48
danstan <oduordanstan@gmail.com>
2023-04-10 23:35:26 +0300

    whatsapp

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - the main entry file
+ * @argc: the int argument
+ * @argv: an array
+ * Return: returns a 0
+ */
+
+int main(int argc, char *argv[])
+{
+/* (void) argc; */
+printf("%s\n", *argv);
+return (0);
+}
