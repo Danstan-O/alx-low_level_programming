6b8476d839b5fcd91f0b0916c987bbdf793c0419
danstan <oduordanstan@gmail.com>
2023-04-13 22:33:33 +0300

    used to concat a string

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,6 +1,5 @@
 #include <stdlib.h>
 #include <stdio.h>
-
 /**
  * string_nconcat - allocates memory to malloc
  * Return: returns a character
@@ -8,8 +7,45 @@
  * @s2: the second input parameter
  * @n: the third input parameter
  */
-
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
+char *check;
+unsigned int l1, l2, l4, i;
 
+if (s1 == NULL)
+{
+s1 = "";
+}
+if (s2 == NULL)
+{
+s2 = "";
+}
+
+for (l1 = 0; s1[l1] != '\0'; l1++)
+;
+for (l2 = 0; s2[l2] != '\0'; l2++)
+;
+if (n >= l2)
+{
+n = l2;
+}
+l4 = l1 + n;
+check = malloc(l4 + 1);
+if (check == NULL)
+{
+return (NULL);
+}
+for (i = 0; i < l4; i++)
+{
+if (i < l1)
+{
+check[i] = s1[i];
+}
+else
+{
+check[i] = s2[i - l1];
+}
+}
+check[i] = '\0';
+return (check);
 }
