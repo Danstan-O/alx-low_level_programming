c6c3525d410f53d34e08c3a04324b2a0709a495b
danstan <oduordanstan@gmail.com>
2023-04-13 15:50:11 +0300

    if statement

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -10,12 +10,17 @@
 
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-char *check = 0;
+char *check;
+unsigned int i;
 
 check = malloc(size * nmemb);
 if (check == NULL || nmemb == 0 || size == 0)
 {
 return (NULL);
+}
+for (i = 0; i < (nmemb * size); i++)
+{
+check[i] = 0;
 }
 return (check);
 }
