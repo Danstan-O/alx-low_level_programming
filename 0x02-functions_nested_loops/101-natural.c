13040ea1640d87cf3c3075cdd5372278b5e488bc
danstan <oduordanstan@gmail.com>
2023-04-15 00:28:05 +0300

    moved everything to main

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -3,28 +3,14 @@
 
 /**
  * main - our main entry
- * @n: our integer input
  * Return: returns 0
  */
-int natural(int n);
 int main(void)
-{
-natural(1023);
-return (0);
-}
-
-/**
- * natural - used to sum up the integers
- * @n: our integer input
- * Return: returns an integer
- */
-
-int natural(int n)
 {
 int i;
 int j = 0;
 
-for (i = 1; i <= n; i++)
+for (i = 1; i <= 1023; i++)
 {
 if (i % 3 == 0 || i % 5 == 0)
 
