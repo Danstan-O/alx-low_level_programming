6984d4f9b6cdd680c279a5b0d3ccac385c5fd6af
danstan <oduordanstan@gmail.com>
2023-04-17 21:49:36 +0300

    include all the prototype headers

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,46 +1,27 @@
 #ifndef DOG_H
 #define DOG_H
-
-#include <stdio.h>
-#include <stdlib.h>
-
 /**
- * struct Dog - a dog's basic info
- * @name: provides name of the dog
- * @age: provides age to the dog
- * @owner: name of the dog owner
- * Description: longer description
- * my_dog - takes in dogs
+ * struct dog - a dog's basic info
+ * @name: First member
+ * @age: Second member
+ * @owner: Third member
+ *
+ * Description: Longer description
  */
-
-struct Dog
+struct dog
 {
 char *name;
 float age;
 char *owner;
 };
-
 /**
- * my_dog - creates a dog
- * @name: provides name of the dog
- * @age: provides age to the dog
- * @owner: name of the dog owner
- * Return: returns the new dog
+ * dog_t - typedef for struct dog
  */
-
-struct Dog *my_dog(char *name, char *owner, float age)
-{
-struct Dog *dog;
-dog = malloc(sizeof(struct Dog));
-if (dog == NULL)
-{
-return (NULL);
-}
-dog->age = age;
-dog->name = name;
-dog->owner = owner;
-
-return (dog);
-}
-
+typedef struct dog dog_t;
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+char *_strcpy(char *dest, char *src);
+int _strlen(char *s);
 #endif
