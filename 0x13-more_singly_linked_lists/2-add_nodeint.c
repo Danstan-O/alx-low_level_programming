d3e0a7b0d47f9a835fa10d038c84287d02f6251a
danstan <oduordanstan@gmail.com>
2023-05-02 22:51:36 +0300

    add data at the beginning

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -1,4 +1,6 @@
 #include "lists.h"
+#include <stdlib.h>
+#include <stdio.h>
 
 /**
  * add_nodeint - adds a node 
@@ -9,5 +11,11 @@
 
 listint_t *add_nodeint(listint_t **head, const int n)
 {
+listint_t *ptr = malloc(sizeof(listint_t));
+ptr->n = n;
+ptr->next = NULL;
 
+ptr->next = *head;
+*head = ptr;
+return (*head);
 }
