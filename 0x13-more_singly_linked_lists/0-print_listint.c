093156a4a2c2283a36f527601bbbb47aedb5a269
danstan <oduordanstan@gmail.com>
2023-05-02 22:38:09 +0300

    created a count element

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -10,10 +10,12 @@
 
 size_t print_listint(const listint_t *h)
 {
-int n = 0;
-n = malloc(sizeof(listint_t));
-
-
-printf("%d", n);
+size_t n = 0;
+while (h != NULL)
+{
+printf("%d\n", h->n);
+n++;
+h = h->next;
+}
 return (n);
 }
