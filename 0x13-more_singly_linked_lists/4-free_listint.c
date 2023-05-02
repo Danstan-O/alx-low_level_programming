d29038429fa27ac351a654008b2a08b9141bc936
danstan <oduordanstan@gmail.com>
2023-05-02 23:24:33 +0300

    freeing memory

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -1,4 +1,6 @@
 #include "lists.h"
+#include <stdlib.h>
+#include <stdio.h>
 
 /**
  * free_listint - adds a node
@@ -8,5 +10,12 @@
 
 void free_listint(listint_t *head)
 {
+listint_t *temp;
+while (head)
+{
+temp = head->next;
+free(head);
+head = temp;
+}
 
 }
