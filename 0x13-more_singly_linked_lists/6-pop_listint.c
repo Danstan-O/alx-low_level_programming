5db848b01ea23f4777948b16a643ad308b5e14ef
danstan <oduordanstan@gmail.com>
2023-05-02 23:45:38 +0300

    free pop

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -1,4 +1,6 @@
 #include "lists.h"
+#include <stdlib.h>
+#include <stdio.h>
 
 /**
  * pop_listint - adds a node
@@ -8,5 +10,17 @@
 
 int pop_listint(listint_t **head)
 {
+listint_t *temp;
+int n;
 
+if (!head || !*head)
+{
+return (0);
+}
+n = (*head)->n;
+temp = (*head)->next;
+free(*head);
+*head = temp;
+
+return (n);
 }
