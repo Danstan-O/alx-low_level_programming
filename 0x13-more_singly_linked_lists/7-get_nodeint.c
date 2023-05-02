20398d5f98d1370f79064c2a7f40092b5f5df606
danstan <oduordanstan@gmail.com>
2023-05-02 23:46:44 +0300

    get node

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -1,4 +1,6 @@
 #include "lists.h"
+#include <stdlib.h>
+#include <stdio.h>
 
 /**
  * get_nodeint_at_index - adds a node
@@ -9,5 +11,14 @@
 
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
+unsigned int i = 0;
+listint_t *temp = head;
 
+while (temp && i < index)
+{
+temp = temp->next;
+i++;
+}
+
+return (temp ? temp : NULL);
 }
