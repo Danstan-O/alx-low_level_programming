c0ee14bce5c335c6b40b9748eb40976c0e6b9507
danstan <oduordanstan@gmail.com>
2023-05-05 00:14:10 +0300

    sets the number of bits

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -1,3 +1,5 @@
+#include "main.h"
+
 /**
  * set_bit - converts a binary number to an integer
  * @n: the parameter input
@@ -7,5 +9,13 @@
 
 int set_bit(unsigned long int *n, unsigned int index)
 {
+unsigned int bit;
 
+if (index > 63)
+{
+return (-1);
+}
+bit = 1 << index;
+*n = (*n | bit);
+return (1);
 }
