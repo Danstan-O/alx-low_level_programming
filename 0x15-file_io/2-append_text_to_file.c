ab928b8863d438c559532ee2ef2cf82d4036c6c1
danstan <oduordanstan@gmail.com>
2023-05-09 00:43:48 +0300

    logic code

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include "main.h"
 
 /**
  * append_text_to_file - our major entry point
@@ -10,5 +11,35 @@
 
 int append_text_to_file(const char *filename, char *text_content)
 {
+int fb, i, rwr;
 
+if (!filename)
+{
+return (-1);
+}
+
+
+fb = open(filename, O_WRONLY | O_APPEND);
+
+if (fb == -1)
+{
+return (-1);
+}
+
+if (text_content)
+{
+for (i = 0; text_content[i]; i++)
+;
+
+rwr = write(fb, text_content, i);
+
+if (rwr == -1)
+{
+return (-1);
+}
+
+}
+
+close(fb);
+return (1);
 }
