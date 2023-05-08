defef152c30eea34b0b160c9f1562c2991091069
danstan <oduordanstan@gmail.com>
2023-05-08 22:52:31 +0300

    wrote my code logic

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include "main.h"
 
 /**
  * read_textfile - our major entry point
@@ -11,4 +12,33 @@
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 
+int fb;
+ssize_t n, nr;
+char *fo;
+
+if (!filename)
+{
+return (0);
+}
+
+
+fb = open(filename, O_RDONLY);
+if (fb == -1)
+{
+return (0);
+}
+
+fo = malloc(sizeof(char) * (letters));
+
+if (!fo)
+{
+return (0);
+}
+
+n = read(fb, fo, letters);
+nr = write(STDOUT_FILENO, fo, n);
+
+close(fb);
+free(fo);
+return (nr);
 }
