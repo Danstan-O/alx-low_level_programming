91beecd8c59a4172822c2001697b978b7f3cb213
danstan <oduordanstan@gmail.com>
2023-05-09 00:06:50 +0300

    create a file permision

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
+#include "main.h"
 
 /**
  * create_file - our major entry point
- * Return: returns the ssize_t
+ * Return: returns the an integer
  * @filename: the name of our file
  * @text_content: our second input
  *
@@ -10,5 +11,35 @@
 
 int create_file(const char *filename, char *text_content)
 {
+int fb, i, r;
 
+if (!filename)
+{
+return (-1);
+}
+
+
+fb = open(filename, O_CREAT | O_TRUNC | O_WRONLY);
+if (fb == -1)
+{
+return (-1);
+}
+
+if (!text_content)
+{
+text_content = "";
+}
+
+for (i = 0; text_content[i]; i++)
+;
+
+r = write(fb, text_content,i);
+
+if (r == -1)
+{
+return (-1);
+}
+
+close(fb);
+return (1);
 }
