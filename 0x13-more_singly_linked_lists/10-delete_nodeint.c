4381012bb94f8c961937eb46bca2e8735104622a
danstan <oduordanstan@gmail.com>
2023-05-20 09:54:28 +0300

    delete node

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -2,12 +2,40 @@
 
 /**
  * delete_nodeint_at_index - adds a node
- * @head: our input
- * @index: index of the node
+ * @head: points to the first element
+ * @index: index of the node to be deleted
  * Return: returns a size_t.
  */
 
 int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
+listint_t *temp = *head;
+listint_t *current = NULL;
+unsigned int i = 0;
 
+if (*head == NULL)
+{
+return (-1);
+}
+
+if (index == 0)
+{
+*head = (*head)->next;
+free(temp);
+return (1);
+}
+
+while (i < index - 1)
+{
+if (!temp || !(temp->next))
+{
+return (-1);
+}
+temp = temp->next;
+i++;
+}
+current = temp->next;
+temp->next = current->next;
+free(current);
+return (1);
 }
